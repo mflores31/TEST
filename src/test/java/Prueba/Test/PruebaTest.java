8b47a52d5180aa04ddfc74f5e201d59caeae7cd5
Josué Miguel Flores Parra <mflores31@uabc.edu.mx>
2019-02-13 00:37:41 -0800

    Primera Versión

---
+++ src/test/java/Prueba/Test/PruebaTest.java
@@ -0,0 +1,23 @@
+package Prueba.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("My Test Class")
+class PruebaTest {
+
+	@Test
+	@DisplayName("My Name")
+	void test() {
+		
+		assertAll("Multiples pruebas", 
+				()-> assertEquals(3,3),
+				()->assertTrue(3>2));
+		
+		assertNotEquals(3, 3, "Error. Datos Iguales");
+		//fail("Not yet implemented");
+	}
+
+}
